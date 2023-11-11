2169813fcdbd0dbd84de73dc134978149b6b4745
Mohamed Gomaa <84819374+mo7meddgom3a@users.noreply.github.com>
2023-11-11 23:33:25 +0200

    fe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from ocr import process_ocr_front, process_ocr_back
-from flask import Flask, render_template, request
+from ocr import process_ocr_front, process_ocr_back ,process_two_sides
+from flask import Flask, render_template, request, url_for
 import os
 import cv2
 
@@ -23,22 +23,38 @@ def upload_image():
         if image.filename != "":
             img_path = os.path.join(UPLOAD_FOLDER_PATH, 'front_id',image.filename)
             image.save(img_path)
+            print(f"  image.filename {image.filename}")
+            print(f" *-front imag path {img_path}")
+            print("type of front img ",type(image))
             osrResult = process_ocr_front(cv2.imread(img_path))
             print(osrResult)
             return {"data":osrResult}
-
+        
+@app.route("/uploadboth", methods=["POST"])
+def upload_image_both():
+    images = request.files.getlist("images")
+    if len(images)==2:
+        img_path1 = os.path.join(UPLOAD_FOLDER_PATH, 'front_id',images[0].filename)
+        print("type of images[0] img ",type(images[0]))
+        images[0].save(img_path1)
+        img_path2 = os.path.join(UPLOAD_FOLDER_PATH, 'back_id',images[1].filename)
+        images[1].save(img_path2)
+        output = process_two_sides(cv2.imread(img_path2),cv2.imread(img_path1))
+        return output
+    else:
+        return {"data":"Only one image is uploaded !"}
+    
 @app.route("/uploadback", methods=["POST"])
 def upload_image_back():
     if "image" in request.files:
         image = request.files["image"]
         if image.filename != "":
            img_path = os.path.join(UPLOAD_FOLDER_PATH, 'back_id',image.filename)
+           print(f" *-back imag path {img_path}")
            image.save(img_path)
            ocrResult = process_ocr_back(cv2.imread(img_path))
            print(ocrResult)
     return {"data":ocrResult}
-
-
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
--- ocr.py
+++ ocr.py
@@ -13,6 +13,8 @@ ocr_front_model = YOLO('./models/final_ocr_front_back.pt')
 # Load a pretrained YOLOv8l model
 ocr_back_model = YOLO("./models/final_ocr_front_back.pt")
 
+face_cascade = cv2.CascadeClassifier('./models/haarcascade_frontalface_default.xml')
+
 
 
 def ocr_reader(img):
@@ -33,6 +35,7 @@ def ocr_reader(img):
     return data
 
 
+
 def read_text_front(img):
     """
     This function take front card image and get the name and address on the card.
@@ -45,9 +48,9 @@ def read_text_front(img):
     """
     
     masks = {
-        "Name": img[110:250, 260:],
-        "Address": img[250:380, 260:]
-        # ,"ID": img[380:500, 335:]
+        "name": img[110:250, 260:],
+        "address": img[250:380, 260:]
+        
     }
 
     Data ={}
@@ -73,10 +76,10 @@ def read_text_back(img):
     img = cv2.resize(img, (1080, 640))
 
     masks = {
-        "Job": img[75:190, 400:],
-        "Gender": img[170:260, 790:900],
-        "Religion": img[175:260, 550:790],
-        "Status":img[175:260, 350:550]
+        "job": img[75:190, 400:],
+        "gender": img[170:260, 790:900],
+        "religion": img[175:260, 550:790],
+        "status":img[175:260, 350:550]
     }
 
     Data ={}
@@ -144,8 +147,8 @@ def read_id_front(img):
     # Extracting gender (odd for male, even for female)
     gender_number = int(result_string[12])
     gender = "ذكر" if gender_number % 2 != 0 else "أنثي"
-
-    return  { "national_id":result ," birthday": birthday, " governorate": governorate, "gender": gender }
+    
+    return  { "national_id":result ,"birthday": birthday, "governorate": governorate, "gender": gender }
 def read_id_back(img):
     """
     This function take back card image and get national number on the card.
@@ -162,10 +165,6 @@ def read_id_back(img):
     cropped = img[0:70, 375:680]
     # Set the confidence threshold for the model to 0.25
     ocr_back_model.conf = 0.25  # Adjust this line according to the actual API
-
-    # Convert cropped image to color (YOLO typically requires three channels)
-    cv2.imshow("",cropped)
-    
     # Run inference on the cropped image
     results = ocr_back_model(cropped)
 
@@ -186,83 +185,83 @@ def read_id_back(img):
     return result
 
 
-# def extract_id_and_average_confidence(img):
-#     """
-#     This function take front card image and get national number
-#     and average precentages of number recognition accurac on the card.
+def extract_id_front_and_average_confidence(img):
+    """
+    This function take front card image and get national number
+    and average precentages of number recognition accurac on the card.
     
-#     Parameters:
-#         src (MatLike)
+    Parameters:
+        src (MatLike)
     
-#     Returns:
-#         id (str)
-#         confidence (float)
-#     """   
-#     # Run inference on the image
-#     results = ocr_front_model(img)
+    Returns:
+        id (str)
+        confidence (float)
+    """   
+    # Run inference on the image
+    cropped = img[380:500, 335:]
+    results = ocr_front_model(cropped)
 
-#     # Extract the bounding boxes, classes, and confidence scores
-#     boxes = results[0].boxes.xywh  # Assuming the boxes are in xywh format
-#     classes = results[0].boxes.cls
-#     confidences = results[0].boxes.conf  # Assuming confidence scores are stored in results[0].boxes.conf
+    # Extract the bounding boxes, classes, and confidence scores
+    boxes = results[0].boxes.xywh  # Assuming the boxes are in xywh format
+    classes = results[0].boxes.cls
+    confidences = results[0].boxes.conf  # Assuming confidence scores are stored in results[0].boxes.conf
 
-#     # Create a list of tuples, where each tuple contains (x-coordinate, class_name, confidence_score)
-#     x_coords_classes_confidences = [
-#         (box[0], ocr_front_model.names[int(cls)], conf) for box, cls, conf in zip(boxes, classes, confidences)
-#     ]
+    # Create a list of tuples, where each tuple contains (x-coordinate, class_name, confidence_score)
+    x_coords_classes_confidences = [
+        (box[0], ocr_front_model.names[int(cls)], conf) for box, cls, conf in zip(boxes, classes, confidences)
+    ]
 
-#     # Sort this list based on the x-coordinate
-#     sorted_info = sorted(x_coords_classes_confidences, key=lambda x: x[0])
+    # Sort this list based on the x-coordinate
+    sorted_info = sorted(x_coords_classes_confidences, key=lambda x: x[0])
     
-#     # Concatenate class names to form the result string
-#     result_string = ''.join([info[1] for info in sorted_info])
+    # Concatenate class names to form the result string
+    result_string = ''.join([info[1] for info in sorted_info])
     
-#     # Calculate the average confidence score
-#     average_confidence = sum([info[2] for info in sorted_info]) / len(sorted_info) if sorted_info else 0
-#     avg = float(average_confidence)
-#     return result_string, avg
+    # Calculate the average confidence score
+    average_confidence = sum([info[2] for info in sorted_info]) / len(sorted_info) if sorted_info else 0
+    avg = float(average_confidence)
+    return result_string, avg
 
-# def extract_id_back_and_average_confidence(img):
-#     """
-#     This function take back card image and get national number
-#     and average percentages of number recognition accuracy on the card.
+def extract_id_back_and_average_confidence(img):
+    """
+    This function take back card image and get national number
+    and average percentages of number recognition accuracy on the card.
     
-#     Parameters:
-#         src (MatLike)
+    Parameters:
+        src (MatLike)
     
-#     Returns:
-#         id (str)
-#         confidence (float)
-#     """   
+    Returns:
+        id (str)
+        confidence (float)
+    """   
     
-#     # Run infer
-#     a, b,_= img.shape
-#     cropped = img[0:65, 375:690]
+    # Run infer
+    cropped = img[0:65, 375:690]
     
-#     # Run inference on the cropped image
-#     results = ocr_back_model(cropped)  # Ensure the model accepts numpy array inputs directly
+    # Run inference on the cropped image
+    results = ocr_back_model(cropped)  # Ensure the model accepts numpy array inputs directly
 
-#     # Extract the bounding boxes and their corresponding classes
-#     # The following lines assume results is a list-like object with attributes, adjust according to actual returned object
-#     boxes = results[0].boxes.xywh  # Assuming the boxes are in xywh format
-#     classes = results[0].boxes.cls
-#     confidences = results[0].boxes.conf  # Assuming confidence scores are stored in results[0].boxes.conf
+    # Extract the bounding boxes and their corresponding classes
+    # The following lines assume results is a list-like object with attributes, adjust according to actual returned object
+    boxes = results[0].boxes.xywh  # Assuming the boxes are in xywh format
+    classes = results[0].boxes.cls
+    confidences = results[0].boxes.conf  # Assuming confidence scores are stored in results[0].boxes.conf
 
-#     # Create a list of tuples, where each tuple contains (x-coordinate, class_name, confidence_score)
-#     x_coords_classes_confidences = [
-#         (box[0], ocr_back_model.names[int(cls)], conf) for box, cls, conf in zip(boxes, classes, confidences)
-#     ]
+    # Create a list of tuples, where each tuple contains (x-coordinate, class_name, confidence_score)
+    x_coords_classes_confidences = [
+        (box[0], ocr_back_model.names[int(cls)], conf) for box, cls, conf in zip(boxes, classes, confidences)
+    ]
 
-# # Sort this list based on the x-coordinate
-#     sorted_info = sorted(x_coords_classes_confidences, key=lambda x: x[0])
+# Sort this list based on the x-coordinate
+    sorted_info = sorted(x_coords_classes_confidences, key=lambda x: x[0])
     
-#     # Concatenate class names to form the result string
-#     result_string = ''.join([info[1] for info in sorted_info])
+    # Concatenate class names to form the result string
+    result_string = ''.join([info[1] for info in sorted_info])
     
-#     # Calculate the average confidence score
-#     average_confidence = sum([info[2] for info in sorted_info]) / len(sorted_info) if sorted_info else 0
-#     avg = float(average_confidence)
-#     return result_string , avg
+    # Calculate the average confidence score
+    average_confidence = sum([info[2] for info in sorted_info]) / len(sorted_info) if sorted_info else 0
+    avg = float(average_confidence)
+    return result_string , avg
 
 
 
@@ -294,53 +293,62 @@ def process_ocr_back(back_img):
         src (MatLike)
     
     Returns:
-        data (dict): {"Job": , "Gender": ,"Religion": ,"Status": , "national_id":}
+        data (dict): {"Job": , "Gender": ,"Religion": ,"Status": , "National_id":}
     """
 
     card_img = crop_card_back(back_img)
     Data = read_text_back(card_img)
-    Data["Id"] = read_id_back(card_img)
-    
+    Data["national_id"] = read_id_back(card_img)
     return Data
 
 
-# def process_ocr_two_side(front_img, back_img):
-#     print("received two images")
-#     # front image
-#     card_front = crop_card_front (front_img)
-#     data_front = read_text_front(card_front)
-#     id_front, avg_confidence_front = extract_id_and_average_confidence(card_front)
+def process_two_sides(image_path1, image_path2):
 
-#     # back image
-#     card_back = crop_card_back(back_img)
-#     data_back = read_text_back(card_back)
-#     id_back, avg_confidence_back = extract_id_back_and_average_confidence(card_back)
+    # Function to detect face in an image
+    def has_face(image_path):
+        faces = face_cascade.detectMultiScale(image_path, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        return len(faces) > 0, image_path
 
-#     # detect the best id
-#     id = id_front
-#     if id_front == id_back and len(id_front) == 14:
-#         id = id_front
+    # Process both images
+    has_face1, image1 = has_face(image_path1)
+    has_face2, image2 = has_face(image_path2)
 
-#     elif len(id_front) == 14 and len(id_back) != 14:
-#         id = id_front
+    # Assign images to variables based on whether they have a face or not
+    image_with_face = image1 if has_face1 else image2 if has_face2 else None
+    image_without_face = image2 if not has_face2 else image1 if not has_face1 else None
+    card_front = crop_card_front (image_with_face)
+    data_front = read_text_front(card_front)
+    id_front, avg_confidence_front = extract_id_front_and_average_confidence(card_front)
+
+    # back image
+    card_back = crop_card_back(image_without_face)
+    data_back = read_text_back(card_back)
+    id_back, avg_confidence_back = extract_id_back_and_average_confidence(card_back)
+
+    # detect the best id
+    id = id_front
+    if id_front == id_back and len(id_front) == 14:
+        id = id_front
+
+    elif len(id_front) == 14 and len(id_back) != 14:
+        id = id_front
     
-#     elif len(id_front) != 14 and len(id_back) == 14:
-#         id = id_back
+    elif len(id_front) != 14 and len(id_back) == 14:
+        id = id_back
     
-#     elif len(id_front) == len(id_back) and len(id_front) == 14:
-#         if avg_confidence_front > avg_confidence_back:
-#             id = id_front
+    elif len(id_front) == len(id_back) and len(id_front) == 14:
+        if avg_confidence_front > avg_confidence_back:
+            id = id_front
+        else:
+            id = id_back
+    else:
+        id = "re take photo"
 
-#         else:
-#             id = id_back
-
-#     else:
-#         id = None
-
-#     # collect all data
-#     Data = {}
-#     Data.update(data_front)
-#     Data.update(data_back)
-#     Data["Id"] = id
-
-#     return Data
+    # collect all data
+    data = {}
+    data.update(data_front)
+    data.update(data_back)
+    data["national_id"] = id
+    print(f"******* data {data}")
+   
+    return data
